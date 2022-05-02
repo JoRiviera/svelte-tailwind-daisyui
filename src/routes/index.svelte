df260a2c526421978f60762bc0f36aa3ae55b585
jca <jonathan@millionvictories.com>
2022-05-02 08:18:12 +0200

    Init

---
+++ src/routes/index.svelte
@@ -0,0 +1,20 @@
+<script context="module">
+	export async function load({ fetch }) {
+		const url = `./api/name`;
+		const response = await fetch(url);
+		const { name } = response.ok && (await response.json());
+		return {
+			status: response.status,
+			props: { name }
+		};
+	}
+</script>
+
+<script>
+	import { HelloWorld } from '$lib';
+	export let name;
+</script>
+
+<h1 class="prose text-4xl text-center my-10">Welcome to the world</h1>
+
+<HelloWorld {name} />
